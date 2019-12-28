08b2fc5c03ea3ca6aa8ada88ccf7c300e715e4a1
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-12-29 00:54:41 +0800

    Create data query for different date intervals

--- src/components/Dashboard.svelte
+++ src/components/Dashboard.svelte
@@ -3,31 +3,117 @@
   import firebase from "firebase/app";
   import "firebase/firestore";
   import { toastMessage } from "../store/store";
+  import ApexCharts from "apexcharts";
 
   let db;
+  let currentInterval = "1M";
+  let queryInterval = getQueryInterval(currentInterval);
+  let totalSpend = 0;
 
   onMount(() => {
-    db = firebase.firestore();
+    if (localStorage.getItem("interval")) {
+      currentInterval = localStorage.getItem("interval");
+    }
 
+    db = firebase.firestore();
+  });
+
+  function changeInterval(interval) {
+    currentInterval = interval;
+    localStorage.setItem("interval", currentInterval);
+    queryInterval = getQueryInterval(currentInterval);
+  }
+
+  function getQueryInterval(interval) {
+    const currentDate = new Date(new Date().setDate(1));
+
+    let queryDate;
+
+    if (interval === "1M") {
+      queryDate = new Date(currentDate.setDate(1));
+    } else if (interval === "6M") {
+      queryDate = new Date(currentDate.setMonth(currentDate.getMonth() - 6));
+    } else if (interval === "1Y") {
+      queryDate = new Date(currentDate.setMonth(0));
+    } else if (interval === "All") {
+      return "1999-01-01";
+    }
+
+    return parseDateString(
+      queryDate.getFullYear(),
+      queryDate.getMonth() + 1,
+      queryDate.getDate()
+    );
+  }
+
+  function parseDateString(year, month, date) {
+    let monthString, dateString;
+
+    if (month < 10) {
+      monthString = "0" + month;
+    } else {
+      monthString = month;
+    }
+
+    if (date < 10) {
+      dateString = "0" + date;
+    } else {
+      dateString = date;
+    }
+
+    return year + "-" + monthString + "-" + dateString;
+  }
+
+  function fetchData(queryInterval) {
     toastMessage.set("Updating...");
 
     db.collection("expenses")
+      .where("date", ">=", queryInterval)
+      .where("date", "<=", new Date().toISOString().substring(0, 10))
       .get()
       .then(querySnapshot => {
         toastMessage.set("");
-        console.log(querySnapshot);
+        querySnapshot.forEach(query => {
+          console.log(query.data());
+        });
       })
       .catch(err => {
         toastMessage.set(err);
         setTimeout(() => toastMessage.set(""), 3000);
       });
-  });
+  }
+
+  $: if (db) fetchData(queryInterval);
 </script>
 
 <style type="text/postcss">
-  .button {
-    @apply bg-blue-700 text-white font-bold py-2 px-4 rounded;
+  .interval-button {
+    @apply rounded-full bg-gray-300 text-white py-1 font-bold w-1/4 flex-grow;
+    transition: background-color 350ms;
+  }
+
+  .interval-button:not(:last-child) {
+    @apply mr-2;
+  }
+
+  .interval-button.active {
+    background-color: hsl(var(--secondary-hue), 50%, 50%);
   }
 </style>
 
-<div class="flex flex-col mx-4 my-8" />
+<div class="flex flex-col mx-4 my-8">
+  <span class="w-full mb-1 text-4xl text-center font-bold">${totalSpend}</span>
+  <span class="w-full text-center text-gray-600 text-sm font-light mb-8">
+    Total spend
+  </span>
+  <div class="w-full h-64 bg-gray-400 mb-8" />
+  <div class="flex flex-row justify-around overflow-x-scroll">
+    {#each ['1M', '6M', '1Y', 'All'] as interval}
+      <button
+        class="interval-button {currentInterval === interval ? 'active' : ''}"
+        on:click={() => changeInterval(interval)}>
+        {interval}
+      </button>
+    {/each}
+  </div>
+</div>
