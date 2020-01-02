15eca474b49b5013dd3ed3bb84465fbfe4d33b55
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2020-01-03 01:53:03 +0800

    Remove hero animation attempted code

--- src/components/CategoryListTile.svelte
+++ src/components/CategoryListTile.svelte
@@ -2,13 +2,12 @@
   import { fade, fly } from "svelte/transition";
   import { onMount } from "svelte";
   import { typeDesigns, handleRouting } from "../util";
-  import { view } from "../store/store";
+  import { view, detailData, originPositions } from "../store/store";
 
   export let data;
   export let index;
 
   let materialIcon, backgroundColor, iconColor, barColor;
-  let buckets = [];
   let expanded = false;
 
   onMount(() => {
@@ -18,49 +17,27 @@
     backgroundColor = "hsl(" + hue + ", 50%, 80%)";
     iconColor = "hsl(" + hue + ", 65%, 40%)";
     barColor = "hsl(" + hue + ", 45%, 95%)";
-
-    data.data.forEach((data, i, arry) => {
-      const year = +data.date.substring(0, 4);
-      const month = +data.date.substring(5, 7);
-      let previousYear, previousMonth;
-
-      if (buckets.length > 0) {
-        previousYear = +buckets[buckets.length - 1].year;
-        previousMonth = +buckets[buckets.length - 1].month;
-      }
-
-      if (i > 0 && year + month !== previousYear + previousMonth) {
-        buckets.push({
-          year: year,
-          month: month
-        });
-      } else if (i === 0) {
-        buckets.push({
-          year: year,
-          month: month
-        });
-      }
-    });
-
-    buckets.sort((first, second) => {
-      if (first.month > second.month) {
-        return 1;
-      } else {
-        return -1;
-      }
-    });
-
-    buckets.sort((first, second) => {
-      if (first.year < second.year) {
-        return 1;
-      } else {
-        return -1;
-      }
-    });
-    console.log(buckets);
   });
 
   function viewDetails() {
+    detailData.set(data);
+
+    const iconPosX = document.getElementById("icon").getBoundingClientRect()
+      .left;
+    const iconPosY = document.getElementById("icon").getBoundingClientRect()
+      .top;
+    const labelPosX = document.getElementById("label").getBoundingClientRect()
+      .left;
+    const labelPosY = document.getElementById("label").getBoundingClientRect()
+      .top;
+
+    originPositions.set({
+      iconX: iconPosX,
+      iconY: iconPosY,
+      labelX: labelPosX,
+      labelY: labelPosY
+    });
+
     handleRouting("detail#" + data.type);
     view.set("detail");
   }
@@ -72,6 +49,7 @@
   out:fade={{ duration: 80 }}
   on:click={() => viewDetails()}>
   <div
+    id="icon"
     class="rounded-full p-2 ml-4 mr-2 fill-current"
     style="background-color: {backgroundColor}; color:{iconColor}">
     <i class="material-icons md-18" style="display:block !important">
@@ -79,7 +57,7 @@
     </i>
   </div>
   <div class="flex flex-col justify-between truncate flex-grow">
-    <span class="font-bold">{data.type}</span>
+    <span class="font-bold" id="label">{data.type}</span>
     <span class="font-light text-gray-600">
       {data.items} {data.items > 1 ? 'entries' : 'entry'}
     </span>
@@ -94,31 +72,3 @@
     style="width: {(window.innerWidth * data.percentage) / 100}px;
     z-index:-1;background-color:{barColor}" />
 </div>
-{#if expanded}
-  {#each buckets as bucket}
-    <div class="p-4">
-      {bucket.year}
-      {new Date(2019, bucket.month - 1, 1).toString().substring(4, 7)}
-    </div>
-  {/each}
-  <!-- {#each data.data as item}
-    <div class="flex flex-row justify-between items-center p-4">
-      <div class="flex flex-col">
-        <div class="flex flex-row flex-grow">
-          <span class="mr-1">
-            {new Date(item.date).toLocaleDateString('default', {
-              day: 'numeric',
-              weekday: 'short',
-              year: '2-digit',
-              month: 'short'
-            })}
-          </span>
-          •
-          <span class="ml-1 mr-2 truncate text-gray-600">{item.addedBy}</span>
-        </div>
-        <span>{item.desc}</span>
-      </div>
-      <div>${item.amount.toFixed(2).toLocaleString()}</div>
-    </div>
-  {/each} -->
-{/if}
--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,11 @@ import { writable } from "svelte/store";
 
 //ui state - 'entry' or 'dashboard' or 'detail' or 'edit'
 export const view = writable("dashboard");
+
+//Handle passing of data  for Detail page and origin points of icon and label for hero animation
+export const detailData = writable({});
+
+//Message to show in toast
 export const toastMessage = writable("");
 
 //user info
