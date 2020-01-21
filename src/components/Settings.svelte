fdf88d83adfd9acdd9fafbd95c8d6aa1404b04ef
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2020-01-21 22:40:49 +0800

    Stretch item tiles to fit entire width in small screens

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -130,48 +130,50 @@
 </style>
 
 <div
-  class="flex flex-col mx-4 my-8 items-center"
+  class="flex flex-col my-8 items-center"
   style="color: var(--text-color)"
   in:fade={{ duration: 80 }}>
   <div class="wrapper">
     {#if Object.keys($userInfo).length === 0}
-      <button
-        class="flex bg-white rounded shadow text-gray-700 py-2 px-4 font-bold
-        justify-center w-1/2 md: w-full"
-        transition:fade={{ duration: 80 }}
-        on:click={() => signIn()}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 48 48"
-          class="w-6 h-6 pr-2">
-          <defs>
+      <div class="flex justify-center">
+        <button
+          class="flex bg-white rounded shadow text-gray-700 py-2 px-4 mx-4
+          font-bold justify-center w-1/2 md: w-auto"
+          transition:fade={{ duration: 80 }}
+          on:click={() => signIn()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 48 48"
+            class="w-6 h-6 pr-2">
+            <defs>
+              <path
+                id="a"
+                d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2
+                0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6
+                4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22
+                0-1.3-.2-2.7-.5-4z" />
+            </defs>
+            <clipPath id="b">
+              <use xlink:href="#a" overflow="visible" />
+            </clipPath>
+            <path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
             <path
-              id="a"
-              d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2
-              0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1
-              29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22
-              0-1.3-.2-2.7-.5-4z" />
-          </defs>
-          <clipPath id="b">
-            <use xlink:href="#a" overflow="visible" />
-          </clipPath>
-          <path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
-          <path
-            clip-path="url(#b)"
-            fill="#EA4335"
-            d="M0 11l17 13 7-6.1L48 14V0H0z" />
-          <path
-            clip-path="url(#b)"
-            fill="#34A853"
-            d="M0 37l30-23 7.9 1L48 0v48H0z" />
-          <path
-            clip-path="url(#b)"
-            fill="#4285F4"
-            d="M48 48L17 24l-4-3 35-10z" />
-        </svg>
-        <span>Sign in with Google</span>
-      </button>
+              clip-path="url(#b)"
+              fill="#EA4335"
+              d="M0 11l17 13 7-6.1L48 14V0H0z" />
+            <path
+              clip-path="url(#b)"
+              fill="#34A853"
+              d="M0 37l30-23 7.9 1L48 0v48H0z" />
+            <path
+              clip-path="url(#b)"
+              fill="#4285F4"
+              d="M48 48L17 24l-4-3 35-10z" />
+          </svg>
+          <span>Sign in with Google</span>
+        </button>
+      </div>
     {:else}
       <div class="flex flex-row items-center px-4">
         <img
@@ -186,8 +188,7 @@
         </div>
       </div>
     {/if}
-    <div
-      class="w-full flex flex-row items-center justify-between mt-12 px-4 py-4">
+    <div class="w-auto flex flex-row items-center justify-between mt-12 p-4">
       <span class="label">Theme</span>
       <div class="flex flex-row">
         <div
@@ -200,7 +201,7 @@
     </div>
     <div
       id="refresh-wrapper"
-      class="w-full flex flex-row items-center justify-between fill-current"
+      class="w-auto flex flex-row items-center justify-between fill-current px-4"
       on:click={() => {
         dashboardShouldReload.set(true);
         handleRouting('dashboard');
@@ -214,7 +215,7 @@
       </i>
     </div>
     <div
-      class="w-full text-center my-8 flex flex-row justify-center"
+      class="w-auto text-center my-8 flex flex-row justify-center"
       style="color: var(--text-color2)">
       <a href="/privacy-policy.html" target="_blank" class="mr-2">
         Privacy policy
