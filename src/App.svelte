1f9549f0e791737c9ac395fb1f5d7445144cc1a4
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2020-01-24 01:20:48 +0800

    Move firebase config to ext file

--- src/App.svelte
+++ src/App.svelte
@@ -19,6 +19,7 @@
   import Entry from "./components/Entry.svelte";
   import Search from "./components/Search.svelte";
   import { handleRouting } from "./util";
+  import { firebaseConfig } from "./config";
 
   // if ("serviceWorker" in navigator) {
   //   navigator.serviceWorker.register("/service-worker.js");
@@ -29,15 +30,7 @@
   let app;
 
   onMount(() => {
-    const firebaseConfig = {
-      apiKey: "AIzaSyAK19fHNBTzkPjyM5CZRMyR1H5LyJ8R69E",
-      authDomain: "expensetracker-fe56d.firebaseapp.com",
-      databaseURL: "https://expensetracker-fe56d.firebaseio.com",
-      projectId: "expensetracker-fe56d",
-      storageBucket: "expensetracker-fe56d.appspot.com",
-      messagingSenderId: "680552838328",
-      appId: "1:680552838328:web:d7d7ef47348fbb3920f04d"
-    };
+    const firebaseConfig = firebaseConfig;
 
     app = firebase.initializeApp(firebaseConfig);
 
